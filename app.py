717af42976afa6a1ec41b15abd9a97e8ec24c419
Daniel Canter <danielcanter2013@gmail.com>
2023-06-05 17:17:56 +0300

    Starting-1

--- app.py
+++ app.py
@@ -10,3 +10,20 @@ from functools import wraps
 from helpers import apology, login_required
 
 app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/")
+def index():
+    return render_template("index.html")
