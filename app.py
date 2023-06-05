b14dbdce0b6d966579f8310fa93051dfa35bf7d2
Daniel Canter <danielcanter2013@gmail.com>
2023-06-05 17:13:27 +0300

    Starting-1

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
-print("hello world")
-print("hello world")
+import os
+
+from flask import Flask
+from flask import flash, redirect, render_template, session, request
+from flask_session import Session
+from cs50 import SQL
+from werkzeug.security import check_password_hash, generate_password_hash
+from functools import wraps
+
+from helpers import apology, login_required
+
+app = Flask(__name__)
